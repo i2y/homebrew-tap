fadf322157327e0d326ab9d9a030875695207ccf
i2y <6240399+i2y@users.noreply.github.com>
2026-01-09 09:45:28 +0900

    Add bkstg cask

---
+++ Casks/bkstg.rb
@@ -0,0 +1,11 @@
+cask "bkstg" do
+  version "0.1.0"
+  sha256 "04dc1bddf84fdf13c3b5b6924fc6057eb2dc5af64f99d2b0b1e69e3f8f0cc294"
+
+  url "https://github.com/i2y/bkstg/releases/download/v#{version}/bkstg.dmg"
+  name "bkstg"
+  desc "Mini IDP - A serverless Internal Developer Portal using local Git repository"
+  homepage "https://github.com/i2y/bkstg"
+
+  app "bkstg.app"
+end

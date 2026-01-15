f7e5bf8cabcec277f3e24197567bd113e924f758
i2y <6240399+i2y@users.noreply.github.com>
2026-01-15 14:56:33 +0900

    Update bkstg to v0.4.3

--- Casks/bkstg.rb
+++ Casks/bkstg.rb
@@ -1,6 +1,6 @@
 cask "bkstg" do
-  version "0.4.2"
-  sha256 "f79e4e4b28ec96b821995f2c85eee15d5ffb2d316d9d9b2599951443b15d071c"
+  version "0.4.3"
+  sha256 "cdd597b60b7c5d491c8b62caa586365eeaa1b49478500ae8e672f4f8e3f0578f"
 
   url "https://github.com/i2y/bkstg/releases/download/v#{version}/bkstg.dmg"
   name "bkstg"
